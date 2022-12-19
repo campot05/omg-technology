aa29ef270b378817a2c6b34aea14188652db3141
campot05 <117174027+campot05@users.noreply.github.com>
2022-12-19 20:34:10 +0000

    Deploying to gh-pages from @ campot05/omg-technology@9f2b7781f1a9011e8c53e7a6789e1fc91521562e 🚀

---
+++ index.a111b7ea.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector(".js-menu-container"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=()=>{const n="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open")};t.addEventListener("click",c),n.addEventListener("click",c),window.matchMedia("(min-width: 768px)").addEventListener("change",(n=>{n.matches&&(e.classList.remove("is-open"),t.setAttribute("aria-expanded",!1),bodyScrollLock.enableBodyScroll(document.body))}))})();
+//# sourceMappingURL=index.a111b7ea.js.map
