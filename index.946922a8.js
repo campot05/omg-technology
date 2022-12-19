9e6ca197f7ac45089003fbe5f039534e7fbc35ff
campot05 <117174027+campot05@users.noreply.github.com>
2022-12-19 00:24:30 +0000

    Deploying to gh-pages from @ campot05/omg-technology@2d0f8d5fbfbce612635db0cc473199b35630eb7c 🚀

---
+++ index.946922a8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[modal-open]"),closeModalBtn:document.querySelector("[modal-close]"),modal:document.querySelector("[modal]")};function o(){e.modal.classList.toggle("is__hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.946922a8.js.map
