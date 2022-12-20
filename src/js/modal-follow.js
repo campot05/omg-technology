3663b376a78ae88c3156bc124d6a6e3a06406598
campot05 <daniil.spectr@gmail.com>
2022-12-20 13:39:48 +0200

    fix subscribe modal

--- src/js/modal-follow.js
+++ src/js/modal-follow.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[modal-open]'),
-    closeModalBtn: document.querySelector('[modal-close]'),
-    modal: document.querySelector('[modal]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
