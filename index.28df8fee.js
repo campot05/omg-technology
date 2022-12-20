4d69c2a39e647b572272db74b96b66d7e775ac86
campot05 <117174027+campot05@users.noreply.github.com>
2022-12-20 11:40:31 +0000

    Deploying to gh-pages from @ campot05/omg-technology@3663b376a78ae88c3156bc124d6a6e3a06406598 🚀

---
+++ index.28df8fee.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is__hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.28df8fee.js.map
--- index.74f416a9.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[modal-open]"),closeModalBtn:document.querySelector("[modal-close]"),modal:document.querySelector("[modal]")};function o(){e.modal.classList.toggle("is__hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.74f416a9.js.map
